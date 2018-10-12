ba3d6214ed1e49f88a397c07b03b2ae1fe7c7d79
Bigmilk120 <sprawdzany96@gmail.com>
2018-10-13 00:47:25 +0200

    Cosmetic changes
    Still not working, showing only when tie. No winning, no losing.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,8 @@
 app.controller('MainController', ['$scope', function($scope) { 
+	$iterator=0;
+	$scope.playable=true;
+	$scope.result="game in progress";
+	
 	$scope.board=[
 		{value:0,
 		src:"blank.jpg"},
@@ -20,60 +24,59 @@ app.controller('MainController', ['$scope', function($scope) {
 		src:"blank.jpg"}
 	];
 	
-	$iterator=0;
 	
 	
-	$scope.change=function(i){
-		if($scope.board[i].value==0){
-			$scope.board[i].value=1;
-			$scope.board[i].src="circle.jpg";
-			if(ifLost(true)){
-				
-			}else{
+	if($scope.playable==true){
+		$scope.change=function(i){
+			if($scope.board[i].value==0){
+				$scope.board[i].value=1;
+				$scope.board[i].src="circle.jpg";
+				$scope.ifLost(1);
+				$scope.temp1="jestem";
 				$iterator++;
 				if($iterator==5){
 					tie();
 				}else{
 					AI_move();
+					$scope.ifLost(2);
+					$scope.temp2=" tutaj"+i;
 				}
-			}
+			}	
+		}
+		
+		function AI_move(){
+			var rand_place;
+			do{
+				rand_place =  Math.floor((Math.random() * 8)); 
+			}while($scope.board[rand_place].value!=0);
+			$scope.board[rand_place].value=2;
+			$scope.board[rand_place].src="cross.jpg";
+		}
+		
+		$scope.ifLost=function(chars){
+			if(($scope.board[0]==chars&&$scope.board[1]==chars&&$scope.board[2]==chars)||
+				($scope.board[2]==chars&&$scope.board[5]==chars&&$scope.board[8]==chars)||
+				($scope.board[0]==chars&&$scope.board[3]==chars&&$scope.board[6]==chars)||
+				($scope.board[6]==chars&&$scope.board[7]==chars&&$scope.board[8]==chars)||
+				($scope.board[1]==chars&&$scope.board[4]==chars&&$scope.board[7]==chars)||
+				($scope.board[3]==chars&&$scope.board[4]==chars&&$scope.board[5]==chars)||
+				($scope.board[0]==chars&&$scope.board[4]==chars&&$scope.board[8]==chars)||
+				($scope.board[2]==chars&&$scope.board[4]==chars&&$scope.board[6]==chars)){
+					$scope.playable=false;
+					if(i==1){
+						$scope.result="Won";
+					}else{
+						$scope.result=="Lost";
+					}
+				}
 		}
 		
 		
-		
-		
-	}
-	
-	function AI_move(){
-		var rand_place;
-		do{
-			rand_place =  Math.floor((Math.random() * 8)); 
-		}while($scope.board[rand_place].value!=0);
-		$scope.board[rand_place].value=2;
-		$scope.board[rand_place].src="cross.jpg";
-	}
-	
-	function ifLost(){
-		if($scope.board[0]==2&&$scope.board[1]==2&&$scope.board[2]==2)
-			return true;
-		if($scope.board[2]==2&&$scope.board[5]==2&&$scope.board[8]==2)
-			return true;
-		if($scope.board[0]==2&&$scope.board[3]==2&&$scope.board[6]==2)
-			return true;
-		if($scope.board[6]==2&&$scope.board[7]==2&&$scope.board[8]==2)
-			return true;
-		if($scope.board[1]==2&&$scope.board[4]==2&&$scope.board[7]==2)
-			return true;
-		if($scope.board[3]==2&&$scope.board[4]==2&&$scope.board[5]==2)
-			return true;
-		if($scope.board[0]==2&&$scope.board[4]==2&&$scope.board[8]==2)
-			return true;
-		if($scope.board[2]==2&&$scope.board[4]==2&&$scope.board[6]==2)
-			return true;
-		return false;
-	}
-	
-	function tie(){
+		function tie(){
+			$scope.result="Tie";
+			$scope.playable=false;
+		}
+	}else{
 		
 		
 	}
